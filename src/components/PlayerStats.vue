0c9fa4c9279d13b45c1224e66d536d3f259407ff
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2022-05-07 21:26:36 +0200

    Added player stats
    Added player stats to the store and modifications and getters too

---
+++ src/components/PlayerStats.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="PlayStats">{{getName}}</div>
+   <div class="PlayStats" id="itemcontainer">{{getItems}}</div>
+   <div class="PlayStats">{{getStr}}</div>
+   <div class="PlayStats">{{getDef}}</div>
+</template>
+
+<script>
+export default {
+    name: 'PlayerStats',
+
+    computed: {
+        getName() {
+            return this.$store.getters.getName;  
+        },
+        getItems() {
+            return this.$store.getters.getItems;
+        },
+        getStr() {
+            return this.$store.getters.getStr;
+        },
+        getDef() {
+            return this.$store.getters.getDef;
+        }
+    }
+}
+
+</script>
+
+<style>
+.PlayStats {
+    text-align: right;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,19 +3,53 @@ import { createStore } from 'vuex'
 const store = createStore({
   state () {
     return {
-      showModal: false
+      showModal: false,
+      PlayerStats: {
+        name: "Myname",
+        items: [
+          "Lókaki",
+          "GoblinHere",
+          "Hős péncél",
+          "NonBinary kard"
+        ],
+        Str: 10,
+        Def: 10
+      }
     }
   },
   mutations: {
     setShowModal (state, show) {
       state.showModal = show;
+    },
+    setName (state, name) {
+      state.name = name;
+    },
+    AddItem (state, item) {
+      state.items.add(item);
+    },
+    ModifyStr(state, modifyBy) {
+      state.Str += modifyBy;
+    },
+    ModifyDef(state, modifyBy) {
+      state.Def += modifyBy;
     }
   },
   getters: {
     showModal (state) {
       return state.showModal
     },
-
+    getName (state) {
+      return state.PlayerStats.name
+    },
+    getItems (state) {
+      return state.PlayerStats.items
+    },
+    getStr (state) {
+      return state.PlayerStats.Str
+    },
+    getDef (state) {
+      return state.PlayerStats.Def
+    }
   }
 })
 
