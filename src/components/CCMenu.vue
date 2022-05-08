1cde0f632d348402a21d8f84d4890d459e7cbad5
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2022-05-08 03:33:56 +0200

    Redesigned CCMenu

--- src/components/CCMenu.vue
+++ src/components/CCMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="CCMenu">
-    <div class="container">
+    <!-- <div class="container">
       <h1>{{ name }}</h1>
       <h2 style="padding-top: 10px;">Nearby cities</h2>
       <div class="ShowHide">
@@ -10,7 +10,7 @@
           <div style="color: green;">
             <div style="margin-top: 5px">Újpest felszabadítása <button @click="onUjClick" id="UjpestButton">\/</button>
             </div>
-            <!-- Make look züd and az mint budapestnél -->
+             Make look züd and az mint budapestnél
             <div id="Ujpest" style="display: none; font-size: 10px;">
               Team: Újpesti Barmok
               <br />
@@ -20,31 +20,49 @@
             </div>
           </div>
           <a  @click="this.$router.push('/map')" id="ZugloButton" style="margin-top: 5px">Zugló szörnye</a>
-          <!-- Normal on click thingie go to map -->
+           Normal on click thingie go to map
           <div style="margin-top: 5px; color:darkgrey; opacity: 50%;">A vár réme</div>
-          <!-- Make gray and low opacity so it's not available yet -->
+        Make gray and low opacity so it's not available yet
         </div>
       </div>
+    </div> -->
+
+
+    <div id="parent">
+      <h1 id="Title">Budapest</h1>
+      <h2 id="PName">{{ getPlayerName }}</h2>
     </div>
-    <div id="MAp" style="display: none;">
-      <AdvenTIERmap />
+
+    <div class="RescueThePricess">
+      <h3 id="RescueHer" @click="this.$router.push('/map2')">Rescue The Princess</h3>
+      <img class="img1" src="./../assets/dragon1.png" alt="" width="52" height="52">
+      <img class="img1" src="./../assets/dragon2.png" alt="" width="52" height="52">
+      <img class="img1" src="./../assets/dragon3.png" alt="" width="52" height="52">
     </div>
+
+    <div class="Challange2">
+      <h3 id="Second" @click="this.$router.push('/map')">Challange2</h3>
+      <img class="img1" src="./../assets/dragon1.png" alt="" width="52" height="52">
+      <img class="img1" src="./../assets/dragon2.png" alt="" width="52" height="52">
+      <img class="img1" src="./../assets/dragon4.png" alt="" width="52" height="52">
+    </div>
+
+    <div class="Challange2">
+      <h3 id="Third">Challange3</h3>
+      <img class="img2" src="./../assets/dragon1.png" alt="" width="52" height="52">
+      <img class="img1" src="./../assets/dragon2.png" alt="" width="52" height="52">
+      <img class="img2" src="./../assets/dragon4.png" alt="" width="52" height="52">
+    </div>
+
   </div>
 </template>
 
 <script>
-var BpHidden = true;
-var UjpestHidden = true;
-
-import AdvenTIERmap from './AdvenTIERmap.vue';
 
 export default {
   name: 'CCMenu',
-  props: {
-    name: String
-  },
   components: {
-    AdvenTIERmap
+
   },
   computed: {
     getPlayerBadges() {
@@ -55,28 +73,97 @@ export default {
         result += ", ";
       });
       return result;
+    },
+    getPlayerName() {
+      return this.$store.getters.getName;
     }
   },
   methods: {
-    onBpClick() {
-      if (BpHidden == false) {
-        document.getElementById("Budapest").style.display = "none"; //Make it disappear
-        BpHidden = true;
-      } else {
-        document.getElementById("Budapest").style.display = "inline"; //Make it appear
-        BpHidden = false;
-      }
-    },
-    onUjClick() {
-      if (UjpestHidden == false) {
-        document.getElementById("Ujpest").style.display = "none"; //Make it disappear
-        UjpestHidden = true;
-      } else {
-        document.getElementById("Ujpest").style.display = "inline"; //Make it appear
-        UjpestHidden = false;
-      }
-    }
+
   }
 }
 </script>
+<style scoped>
+.CCMenu {
+  background-image: url(@/assets/bg.png);
+  width: 100%;
+  height: 480px;
+}
+#parent {
+  /* margin-top: 15px; */
+  width: 100%;
+  height: 90px;
+  white-space: nowrap;
+  overflow-x: auto;
+}
+
+#Title {
+  margin-top: 15px;
+  text-align: right;
+  display: inline-block;
+  width: 55%;
+  height: 100%;
+}
+
+#PName {
+  margin-top: 15px;
+  text-align: right;
+  display: inline-block;
+  width: 45%;
+  height: 100%;
+}
+
+.RescueThePricess {
+  padding-left: 20px;
+  width: 100%;
+  height: 90px;
+  white-space: nowrap;
+  overflow-x: auto;
+}
+
+#RescueHer {
+  padding-top: 5px;
+  display: inline-block;
+  width: 40%;
+  text-align:center;
+}
+
+.img1 {
+  display: inline-block;
+  width: 110px;
+  height: 90px;
+  opacity: 25%;
+}
+
+.Challange2 {
+  padding-top: 10px;
+  padding-left: 20px;
+  width: 100%;
+  height: 100px;
+  white-space: nowrap;
+  overflow-x: auto;
+}
+
+#Second {
+  padding-top: 5px;
+  display: inline-block;
+  width: 40%;
+  text-align: center;
+  padding-right: 100px;
+}
+
+#Third {
+  padding-top: 5px;
+  display: inline-block;
+  width: 40%;
+  text-align: center;
+  padding-right: 100px;
+}
+.img2 {
+  display: inline-block;
+  width: 110px;
+  height: 90px;
+  opacity: 100%;
+}
+</style>
 
