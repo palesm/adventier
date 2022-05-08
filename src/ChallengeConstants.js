99662038a04fc4e6b552e55971318c032ec16d4c
mariannpales <mariann.pales@zooshgroup.com>
2022-05-08 02:02:17 +0200

    lint fix

--- src/ChallengeConstants.js
+++ src/ChallengeConstants.js
@@ -1,15 +1,14 @@
-
 const ChallengeConstants = {
   end: {
     title: "Challenge at this location ended",
     //wiki data
     wikiInfo: ``,
-    photo: '@assets/heroes-sqr',
+    photo: "@assets/heroes-sqr",
     //challenge data
     question: "close this modal",
     answers: [],
     resolutions: [],
-    scenePhoto: '@assets/heroes-sqr',
+    scenePhoto: "@assets/heroes-sqr",
   },
   heroes: {
     title: "Hősök tere",
@@ -22,11 +21,11 @@ const ChallengeConstants = {
     Unknown Soldier. The square lies at the outbound end of Andrássy Avenue
     next to City Park (Városliget). It hosts the Museum of Fine Arts and the
     Palace of Art (Műcsarnok).`,
-    photo: '@assets/heroes-sqr',
+    photo: "@assets/heroes-sqr",
     //challenge data
     question: "meghágod-e a sárkányt?",
     answers: ["naná", "persze", "hogyne"],
-    scenePhoto: '@assets/heroes-sqr',
+    scenePhoto: "@assets/heroes-sqr",
   },
   bazilika2: {
     title: "Szent István Bazilika",
@@ -38,11 +37,11 @@ const ChallengeConstants = {
     Unknown Soldier. The square lies at the outbound end of Andrássy Avenue
     next to City Park (Városliget). It hosts the Museum of Fine Arts and the
     Palace of Art (Műcsarnok).`,
-    wikiPhoto: '/assets/heroes-sqr',
+    wikiPhoto: "/assets/heroes-sqr",
     question: "meghágod-e a sárkányt?",
     answers: ["naná", "persze", "hogyne"],
     resolutions: ["ügyi voltál", "meghaltál", "szexelhetsz a sárkánnyal"],
-    nextModal: ["end","end", "end" ],
+    nextModal: ["end", "end", "end"],
   },
   //ANDRIS
   halaszbastya: {
@@ -54,10 +53,10 @@ const ChallengeConstants = {
     Budapest, Hungary. Needless to say, there used to be real castle walls
      where now you can take fantastic photos from, but the present day 
      structure has never served as an actual fortification in Buda.`,
-    photo: '@assets/fuligjimmy',
+    photo: "@assets/fuligjimmy",
     question: "feladat",
     password: "pw",
-    scenePhoto: '@assets/fisherman'
+    scenePhoto: "@assets/fisherman",
   },
   bazilika: {
     title: "St. Stephen Basilica",
@@ -70,10 +69,10 @@ const ChallengeConstants = {
           co-cathedral of the Roman Catholic Archdiocese of 
           Esztergom-Budapest. Today, it is the third largest church 
           building in present-day Hungary.`,
-          photo: '@assets/senkialfonz',
-          question: "feladat",
-          password: "pw",
-          scenePhoto: '/assets/ststephen.jpg'
+    photo: "@assets/senkialfonz",
+    question: "feladat",
+    password: "pw",
+    scenePhoto: "/assets/ststephen.jpg",
   },
   parlament: {
     title: "Parlament",
@@ -83,10 +82,10 @@ const ChallengeConstants = {
       its location,[5] is the seat of the National Assembly of Hungary, 
       a notable landmark of Hungary, and a popular tourist destination 
       in Budapest.`,
-    wikiPhoto: '/assets/piszkosfred.jpg',
+    wikiPhoto: "/assets/piszkosfred.jpg",
     question: "feladat",
     password: "pw",
-    challengePhoto: '/assets/parlament.jpg'
+    challengePhoto: "/assets/parlament.jpg",
   },
   budavar: {
     title: "Budavári Palota",
@@ -96,10 +95,10 @@ const ChallengeConstants = {
      in Budapest. It was first completed in 1265, although the massive 
      Baroque palace today occupying most of the site was built between
       1749 and 1769.`,
-      photo: '@assets/piszkosfred',
-      question: "feladat",
-      password: "pw",
-      scenePhoto: '@assets/parlament'
+    photo: "@assets/piszkosfred",
+    question: "feladat",
+    password: "pw",
+    scenePhoto: "@assets/parlament",
   },
   //(S)lay the Dragon
 
@@ -114,47 +113,47 @@ const ChallengeConstants = {
     Unknown Soldier. The square lies at the outbound end of Andrássy Avenue
     next to City Park (Városliget). It hosts the Museum of Fine Arts and the
     Palace of Art (Műcsarnok).`,
-    photo: '@assets/heroes-sqr',
+    photo: "@assets/heroes-sqr",
     //challenge data
     question: `he greatest kings, leaders and statesman of Hungary. 
     Their might pours over you as you stand before them. Jhon Hunyadi, the champion of Christ steps forward, raising his sword, he questions you:
     "Who brought God’s voice to this land? Whom among us was crowned on the ice of Danube?"
     `,
-    answers: [
-      "Saint Ladislas",
-      "King Saint Stephen",
-      "Coloman the Learned"],
-    scenePhoto: '@assets/heroes-sqr',
+    answers: ["Saint Ladislas", "King Saint Stephen", "Coloman the Learned"],
+    scenePhoto: "@assets/heroes-sqr",
     resolutions: [
       `“It was King Saint Stephen. Gather all your wits together, young knight, you will need it” He seathes his sword and steps back to his original position.`,
       `"You face a great Foe, may all of our might be with You!” says John, and hands over his sword to you.`,
-      `“It was King Saint Stephen. Gather all your wits together, young knight, you will need it” He seathes his sword and steps back to his original position.`],
+      `“It was King Saint Stephen. Gather all your wits together, young knight, you will need it” He seathes his sword and steps back to his original position.`,
+    ],
     nextModal: ["end", "end", "end"],
   },
   Chapel: {
     title: "Chapel",
     //wiki data
     wikiInfo: `TODO`,
-    photo: '@assets/heroes-sqr',
+    photo: "@assets/heroes-sqr",
     //challenge data
     question: `The Priest offers his blessings to you as you tell him your goals.
     `,
     answers: [
-      "Boon of the Knight", 
-      "Boon of the Scholar", 
-      "Boon of the Ascetic"],
-    scenePhoto: '@assets/heroes-sqr',
+      "Boon of the Knight",
+      "Boon of the Scholar",
+      "Boon of the Ascetic",
+    ],
+    scenePhoto: "@assets/heroes-sqr",
     resolutions: [
-     "You fell stronger",
-     "You fell wiser", 
-     "You feel more durable"],
-    nextModal: ["end", "end", "end"]
+      "You fell stronger",
+      "You fell wiser",
+      "You feel more durable",
+    ],
+    nextModal: ["end", "end", "end"],
   },
   Anonymus: {
     title: "Anonymus",
     //wiki data
     wikiInfo: `TODO`,
-    photo: '@assets/heroes-sqr',
+    photo: "@assets/heroes-sqr",
     //challenge data
     question: `Before you a hooded figure, Bele Regis Notarius, 
     the great chronicler stands. As you approach, he leans forward, 
@@ -165,17 +164,18 @@ const ChallengeConstants = {
     “I tell you one secret , but choose wisely as your life depends on it”
     `,
     answers: [
-      "How can one slay the dragon with one strike", 
-      "Why the dragon kidnapped princess Matilda", 
-      "Whats the dragon greatest fear"],
-    scenePhoto: '@assets/heroes-sqr',
+      "How can one slay the dragon with one strike",
+      "Why the dragon kidnapped princess Matilda",
+      "Whats the dragon greatest fear",
+    ],
+    scenePhoto: "@assets/heroes-sqr",
     resolutions: [
-     " Her scales is damaged under her front right leg, one good strike, and you can easily reach the beasts heart.",
-     "Her name is Parga and she is lonely, our princess helped a lot of maiden to find true love, and Parga seek her council", 
-     "She is afraid of goats, as their eyes are mirror of the evil"],
-    nextModal: ["end", "end", "end"]
-  }
-
-}
+      " Her scales is damaged under her front right leg, one good strike, and you can easily reach the beasts heart.",
+      "Her name is Parga and she is lonely, our princess helped a lot of maiden to find true love, and Parga seek her council",
+      "She is afraid of goats, as their eyes are mirror of the evil",
+    ],
+    nextModal: ["end", "end", "end"],
+  },
+};
 
 export default ChallengeConstants;
